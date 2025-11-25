511ab4127ba87560f6a14e5e79d0feb9845e0aa4
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2025-11-25 21:31:41 +0530

    function

---
+++ function.java
@@ -0,0 +1,9 @@
+class Main
+{
+    public static void main(String[] args){
+        int arr[]={1,2,34,56};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
