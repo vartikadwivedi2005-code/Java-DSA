f9bdf6fe96d9e6e508854a1321d762b408a8d33f
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-04-10 14:03:27 +0530

    Lecture 1

---
+++ lect_1/function.java
@@ -0,0 +1,10 @@
+//function
+class Main
+{
+    public static void main(String[] args){
+        int arr[]={1,2,34,56};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ lect_1/scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
