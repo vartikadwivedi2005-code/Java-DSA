2361f1060f6dd24e57af5043ab4ce39ea8cfd189
vartikadwivedi2005-code <vartikadwivedi2005@gmail.com>
2026-04-15 19:35:11 +0530

    Lecture 25

---
+++ lect_25/evensum.java
@@ -0,0 +1,15 @@
+package lect_25;
+
+public class evensum {
+    static int evensum1(int[] arr , int i){
+        if(i == arr.length)
+            return 0;
+        int val = (arr[i]%2==0) ? arr[i] : 0;
+        return val + evensum1(arr,i+1);
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int i = 0;
+        System.out.println(evensum1(arr,i));
+    }
+}
